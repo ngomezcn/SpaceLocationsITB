5193ab9bd693719938473071a516835e4fc90757
naimgomezcn@gmail.com <naimgomezcn@gmail.com>
2023-02-14 20:57:18 +0100

    .

--- app/src/main/java/com/example/spacelocations/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/spacelocations/viewmodel/ViewModel.kt
@@ -4,9 +4,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.spacelocations.models.Position.MarkerModel
 import com.example.spacelocations.models.Position.Position
+import kotlin.random.Random
 
 class ViewModel : ViewModel() {
-    var markerList = MutableLiveData<MutableList<MarkerModel>>(mutableListOf())
+    var markerList = MutableLiveData<MutableList<MarkerModel>>(mutableListOf(
+        MarkerModel(
+            //Position(41.4534227,2.1841046),
+            Position(Random.nextDouble(0.0, 40.0) , Random.nextDouble(0.0, 40.0) ),
+            binding.titleEditText.text.toString(),
+            binding.descriptionEditText.text.toString(),
+            cate
+        )
+
+    ))
 
 
     init {
